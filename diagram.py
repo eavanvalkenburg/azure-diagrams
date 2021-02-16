d4eb26ca81275b4adab9fefcbf6adf9272f12319
Eduard van Valkenburg <github@vanvalkenburg.eu>
2021-02-16 12:16:47 +0000

    colors added

--- diagram.py
+++ diagram.py
@@ -12,13 +12,17 @@ with Diagram("ML integrated into website", show=False, curvestyle="ortho"):
         ml_ws = MachineLearningServiceWorkspaces("ML Workspace")
         acr = ContainerRegistries("Container Registry")
         modelling = [ml_ws, acr]
-        storage >> ml_ws >> Edge(label="Image with trained model") >> acr
+        storage >> Edge(label="Training data", color="blue") >> ml_ws >> Edge(
+            label="Image with trained model", style="dashed"
+        ) >> acr
 
     with Cluster("Data pipeline"):
         cache = CacheForRedis("Real-time DB")
         cache_function = FunctionApps("Update Cache Function")
         data = [cache_function, cache]
-        storage >> Edge(label="Daily") >> cache_function >> Edge(label="Daily") >> cache
+        storage >> Edge(label="Daily", color="blue") >> cache_function >> Edge(
+            label="Daily", color="blue"
+        ) >> cache
 
     with Cluster("Prediction pipeline"):
         website = AppServiceEnvironments("Website")
@@ -26,10 +30,10 @@ with Diagram("ML integrated into website", show=False, curvestyle="ortho"):
         predict_function = FunctionApps("User profile enrichment")
         prediction = [website, predict_function, scoring_container]
         scoring_container >> Edge(
-            label="Prediction", forward=True, reverse=True
+            label="Prediction", forward=True, reverse=True, color="green"
         ) >> predict_function >> Edge(
-            label="Prediction", forward=True, reverse=True
+            label="Prediction", forward=True, reverse=True, color="green"
         ) >> website
 
     acr >> Edge(style="dashed", label="Deploy image") >> scoring_container
-    predict_function >> Edge(forward=True, reverse=True) >> cache
+    predict_function >> Edge(forward=True, reverse=True, color="green") >> cache
